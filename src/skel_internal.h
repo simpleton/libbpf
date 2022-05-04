61740616c80abf773ca285ce288dc490ac201b67
Sim Sun <simsun@fb.com>
2022-05-03 17:27:33 -0700

    moneky patch for build o10-pahole

--- src/skel_internal.h
+++ src/skel_internal.h
@@ -8,12 +8,26 @@
 #include <sys/mman.h>
 
 #ifndef __NR_bpf
-# if defined(__mips__) && defined(_ABIO32)
+# if defined(__i386__)
+#  define __NR_bpf 357
+# elif defined(__x86_64__)
+#  define __NR_bpf 321
+# elif defined(__aarch64__)
+#  define __NR_bpf 280
+# elif defined(__sparc__)
+#  define __NR_bpf 349
+# elif defined(__s390__)
+#  define __NR_bpf 351
+# elif defined(__arc__)
+#  define __NR_bpf 280
+# elif defined(__mips__) && defined(_ABIO32)
 #  define __NR_bpf 4355
 # elif defined(__mips__) && defined(_ABIN32)
 #  define __NR_bpf 6319
 # elif defined(__mips__) && defined(_ABI64)
 #  define __NR_bpf 5315
+# else
+#  error __NR_bpf not defined. libbpf does not support your arch.
 # endif
 #endif
 
